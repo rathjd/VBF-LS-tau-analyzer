a277e63001b9c0f800bdcb65357ec9d602051df7
Denis Rathjens <Denis.Rathjens@cern.ch>
2013-08-14 11:08:09 +0200

    added methods for scale correction and loose variants

--- effmap_macro.C
+++ effmap_macro.C
@@ -17,6 +17,24 @@ TFile _file0 ("effmap.root","UPDATE");
 
 ((TH2F*)(_file0->Get("h2_taufakerate_eff")))->Write();
 
+((TH2F*)(_file0->Get("h2_taufakerate_loose_num")))->Clone("h2_taufakerate_loose_eff");
+
+((TH2F*)(_file0->Get("h2_taufakerate_loose_eff")))->Divide((TH2F*)(_file0->Get("h2_taufakerate_den")));
+
+((TH2F*)(_file0->Get("h2_taufakerate_loose_eff")))->Write();
+
+((TH1F*)(_file0->Get("h1_taufakescale_num")))->Clone("h1_taufakescale_fac");
+
+((TH1F*)(_file0->Get("h1_taufakescale_fac")))->Divide((TH1F*)(_file0->Get("h1_taufakescale_den")));
+
+((TH1F*)(_file0->Get("h1_taufakescale_fac")))->Write();
+
+((TH1F*)(_file0->Get("h1_taufakescale_loose_num")))->Clone("h1_taufakescale_loose_fac");
+
+((TH1F*)(_file0->Get("h1_taufakescale_loose_fac")))->Divide((TH1F*)(_file0->Get("h1_taufakescale_loose_den")));
+
+((TH1F*)(_file0->Get("h1_taufakescale_loose_fac")))->Write();
+
 cout<<"end"<<endl;
 
 }
