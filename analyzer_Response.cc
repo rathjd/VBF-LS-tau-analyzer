c2cd324cde607b80d9737ffcff2828cf6bf0d440
Denis Rathjens <Denis.Rathjens@cern.ch>
2013-10-04 14:21:53 +0200

    added medium, loose and noIso responses

--- analyzer_Response.cc
+++ analyzer_Response.cc
@@ -90,36 +90,57 @@ int main(int argc, char** argv)
   double ptedges[12]={0.,30.,45.,55.,75.,100.,250.,400.,600.,900.,1500.,2500.};
   double respEdges[29]={0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95,1.00,1.05,1.10,1.15,1.20,1.25,1.30,1.35,1.40,1.45,1.50};
 
+  TH2F* h2_tauResponseN = new TH2F("h2_tauResponseN", "NoIso #tau response", 11, ptedges, 60, 0, 3);
+  TH2F* h2_tauResponseL = new TH2F("h2_tauResponseL", "LooseIso #tau response", 11, ptedges, 60, 0, 3);
+  TH2F* h2_tauResponseM = new TH2F("h2_tauResponseM", "MediumIso #tau response", 11, ptedges, 60, 0, 3);
+  TH2F* h2_tauResponseT = new TH2F("h2_tauResponseT", "TightIso #tau response", 11, ptedges, 60, 0, 3); 
+  h2_tauResponseN->Sumw2();
+  h2_tauResponseL->Sumw2();  
+  h2_tauResponseM->Sumw2();
+  h2_tauResponseT->Sumw2();
+  h2_tauResponseN->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h2_tauResponseN->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h2_tauResponseL->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h2_tauResponseL->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h2_tauResponseM->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h2_tauResponseM->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h2_tauResponseT->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h2_tauResponseT->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
   
-  TH2F* h2_tauResponse_loose = new TH2F("h2_tauResponse_loose", "h2_tauResponse_loose", 11, ptedges, 60, 0, 3);
-  TH2F* h2_tauResponse = new TH2F("h2_tauResponse", "h2_tauResponse", 11, ptedges, 60, 0, 3);  
-  h2_tauResponse_loose->Sumw2();
-  h2_tauResponse->Sumw2();  
-  h2_tauResponse->GetXaxis()->SetTitle("p_{T}^{jet}");
-  h2_tauResponse->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
-  h2_tauResponse_loose->GetXaxis()->SetTitle("p_{T}^{jet}");
-  h2_tauResponse_loose->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  TH3F* h3_tauCompleteN = new TH3F("h3_tauCompleteN","NoIso #tau response", 11, ptedges, 28, respEdges, 4, etaedges); 
+  h3_tauCompleteN->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h3_tauCompleteN->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h3_tauCompleteN->GetZaxis()->SetTitle("|#eta|");
+  h3_tauCompleteN->Sumw2();
   
-  TH3F* h3_tauComplete = new TH3F("h3_tauComplete","h3_tauComplete", 11, ptedges, 28, respEdges, 4, etaedges); 
-  h3_tauComplete->GetXaxis()->SetTitle("p_{T}^{jet}");
-  h3_tauComplete->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
-  h3_tauComplete->GetZaxis()->SetTitle("|#eta|");
-  h3_tauComplete->Sumw2();
+  TH3F* h3_tauCompleteL = new TH3F("h3_tauCompleteL","LooseIso #tau response", 11, ptedges, 28, respEdges, 4, etaedges); 
+  h3_tauCompleteL->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h3_tauCompleteL->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h3_tauCompleteL->GetZaxis()->SetTitle("|#eta|");
+  h3_tauCompleteL->Sumw2();  
   
-  TH3F* h3_tauComplete_loose = new TH3F("h3_tauComplete_loose","h3_tauComplete_loose", 11, ptedges, 28, respEdges, 4, etaedges); 
-  h3_tauComplete_loose->GetXaxis()->SetTitle("p_{T}^{jet}");
-  h3_tauComplete_loose->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
-  h3_tauComplete_loose->GetZaxis()->SetTitle("|#eta|");
-  h3_tauComplete_loose->Sumw2();  
+  TH3F* h3_tauCompleteM = new TH3F("h3_tauCompleteM","MediumIso #tau response", 11, ptedges, 28, respEdges, 4, etaedges); 
+  h3_tauCompleteM->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h3_tauCompleteM->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h3_tauCompleteM->GetZaxis()->SetTitle("|#eta|");
+  h3_tauCompleteM->Sumw2();
   
+  TH3F* h3_tauCompleteT = new TH3F("h3_tauCompleteT","LooseIso #tau response", 11, ptedges, 28, respEdges, 4, etaedges); 
+  h3_tauCompleteT->GetXaxis()->SetTitle("p_{T}^{jet}");
+  h3_tauCompleteT->GetYaxis()->SetTitle("p_{T}^{#tau}/p_{T}^{jet}");
+  h3_tauCompleteT->GetZaxis()->SetTitle("|#eta|");
+  h3_tauCompleteT->Sumw2();  
+    
   //---------------------------------------------------------------------------
   // Histogram Collection Init
   //---------------------------------------------------------------------------
 
         
 
-	MyEventCollection mainObjectSelectionCollection ("mainObjectSelection");
+	MyEventCollection TauTightIsoObjectSelectionCollection ("TauTightIsoObjectSelection");
+	MyEventCollection TauMediumIsoObjectSelectionCollection ("TauMediumIsoObjectSelection");
 	MyEventCollection TauLooseIsoObjectSelectionCollection ("TauLooseIsoObjectSelection");
+	MyEventCollection TauNoIsoObjectSelectionCollection ("TauNoIsoObjectSelection");
 	MyEventCollection JetLooseIsoObjectSelectionCollection ("JetLooseIsoObjectSelection");
 
   //---------------------------------------------------------------------------
@@ -151,7 +172,10 @@ int main(int argc, char** argv)
           if(!(         vertex.size() > 0                                 )) goodVertex = false;
 	  if(goodVertex){
 	    if( goodVertex ) {
-		mainObjectSelectionCollection.goodVertex = true;
+		TauTightIsoObjectSelectionCollection.goodVertex = true;
+		TauMediumIsoObjectSelectionCollection.goodVertex = true;
+		TauLooseIsoObjectSelectionCollection.goodVertex = true;
+		TauNoIsoObjectSelectionCollection.goodVertex = true;
 	    }
 	  }
 
@@ -165,7 +189,10 @@ int main(int argc, char** argv)
                 (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v4 == 1) ||
                 (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v6 == 1)
 
-             ) { mainObjectSelectionCollection.passedTrigger = true;
+             ) { TauTightIsoObjectSelectionCollection.passedTrigger = true;
+	         TauMediumIsoObjectSelectionCollection.passedTrigger = true;
+	         TauLooseIsoObjectSelectionCollection.passedTrigger = true;
+	         TauNoIsoObjectSelectionCollection.passedTrigger = true;
                }
 
           //smart tau selection
@@ -176,8 +203,10 @@ int main(int argc, char** argv)
             if(!(       tau[t].tauID_againstElectronTightMVA3 > 0.5                				)) continue;
             if(!(       tau[t].tauID_againstMuonTight2 > 0.5                        				)) continue;
             if(!(       (tau[t].tauID_decayModeFinding > 0.5) && (tau[t].signalPFChargedHadrCands_size == 1)	)) continue;
-	    if(!(tau[t].tauID_byTightCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) mainObjectSelectionCollection.tau.push_back(&tau[t]);
-	    if(!(tau[t].tauID_byLooseCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) TauLooseIsoObjectSelectionCollection.tau.push_back(&tau[t]);
+	    TauNoIsoObjectSelectionCollection.tau.push_back(&tau[t]);
+	    if(!(tau[t].tauID_byTightCombinedIsolationDeltaBetaCorr3Hits  <= 0.5))  TauTightIsoObjectSelectionCollection.tau.push_back(&tau[t]);
+	    if(!(tau[t].tauID_byMediumCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) TauMediumIsoObjectSelectionCollection.tau.push_back(&tau[t]);
+	    if(!(tau[t].tauID_byLooseCombinedIsolationDeltaBetaCorr3Hits  <= 0.5))  TauLooseIsoObjectSelectionCollection.tau.push_back(&tau[t]);
           }
 
           // jet baseline selection
@@ -188,7 +217,10 @@ int main(int argc, char** argv)
 	}
 
 	  //MET selection
-	  mainObjectSelectionCollection.met.push_back(&met[0]);
+	  TauTightIsoObjectSelectionCollection.met.push_back(&met[0]);
+	  TauMediumIsoObjectSelectionCollection.met.push_back(&met[0]);
+	  TauLooseIsoObjectSelectionCollection.met.push_back(&met[0]);
+	  TauNoIsoObjectSelectionCollection.met.push_back(&met[0]);
 
 	//Event Count
 	ofile.count("NoCuts");
@@ -199,23 +231,35 @@ int main(int argc, char** argv)
 
 	for(unsigned int j = 0;j<JetLooseIsoObjectSelectionCollection.jet.size();++j){	
 		double jetMindR=JetJetMinDistance(JetLooseIsoObjectSelectionCollection,JetLooseIsoObjectSelectionCollection.jet[j]->eta,JetLooseIsoObjectSelectionCollection.jet[j]->phi);
-		pair <double, unsigned int>deltaR = TauJetMinDistanceExtended(mainObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
+		pair <double, unsigned int>deltaRt = TauJetMinDistanceExtended(TauTightIsoObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
+		pair <double, unsigned int>deltaRm = TauJetMinDistanceExtended(TauMediumIsoObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
+		pair <double, unsigned int>deltaRl = TauJetMinDistanceExtended(TauLooseIsoObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
+		pair <double, unsigned int>deltaRn = TauJetMinDistanceExtended(TauNoIsoObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
 		if(jetMindR > 0.5){ //only count isolated jets
-		  if( deltaR.first < 0.1 ){
-			h2_tauResponse->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, mainObjectSelectionCollection.tau[deltaR.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
-			h3_tauComplete->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, mainObjectSelectionCollection.tau[deltaR.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
+		  if( deltaRt.first < 0.1 ){
+			h2_tauResponseT->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauTightIsoObjectSelectionCollection.tau[deltaRt.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
+			h3_tauCompleteT->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauTightIsoObjectSelectionCollection.tau[deltaRt.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
 		  }	
-		  pair <double, unsigned int>deltaRloose = TauJetMinDistanceExtended(TauLooseIsoObjectSelectionCollection, JetLooseIsoObjectSelectionCollection.jet[j]->eta, JetLooseIsoObjectSelectionCollection.jet[j]->phi);
-		  if( deltaRloose.first < 0.1 ){
-			h2_tauResponse_loose->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauLooseIsoObjectSelectionCollection.tau[deltaRloose.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
-			h3_tauComplete_loose->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt,TauLooseIsoObjectSelectionCollection.tau[deltaRloose.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
+		  if( deltaRm.first < 0.1 ){
+			h2_tauResponseM->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauMediumIsoObjectSelectionCollection.tau[deltaRm.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
+			h3_tauCompleteM->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauMediumIsoObjectSelectionCollection.tau[deltaRm.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
+		  }		  
+		  if( deltaRl.first < 0.1 ){
+			h2_tauResponseL->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauLooseIsoObjectSelectionCollection.tau[deltaRl.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
+			h3_tauCompleteL->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauLooseIsoObjectSelectionCollection.tau[deltaRl.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
 		  }
+		  if( deltaRn.first < 0.1 ){
+			h2_tauResponseN->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauNoIsoObjectSelectionCollection.tau[deltaRn.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt);
+			h3_tauCompleteN->Fill(JetLooseIsoObjectSelectionCollection.jet[j]->pt, TauNoIsoObjectSelectionCollection.tau[deltaRn.second]->pt/JetLooseIsoObjectSelectionCollection.jet[j]->pt,fabs(JetLooseIsoObjectSelectionCollection.jet[j]->eta));
+		  }		  
 		}
 	}
 	
 	//Clearing Object Collections 
-	mainObjectSelectionCollection.clear();
+	TauTightIsoObjectSelectionCollection.clear();
+	TauMediumIsoObjectSelectionCollection.clear();
 	TauLooseIsoObjectSelectionCollection.clear();
+	TauNoIsoObjectSelectionCollection.clear();
 	JetLooseIsoObjectSelectionCollection.clear();
 	}
 
