aa1032ae791b3592b93a7ee8e4ca72d0c14c1c4b
Denis Rathjens <Denis.Rathjens@cern.ch>
2014-03-18 12:13:32 +0100

    analyzer for getting trigger efficiencies from WtoLnu-MC

---
+++ analyzer_TriggerWjet.cc
@@ -0,0 +1,241 @@
+//-----------------------------------------------------------------------------
+// File:        analyzer.cc
+// Description: Analyzer for ntuples created by TheNtupleMaker
+// Created:     Fri Apr  5 13:23:28 2013 by mkntanalyzer.py
+// Author:      Daniele Marconi
+//-----------------------------------------------------------------------------
+
+#ifdef PROJECT_NAME
+#include "PhysicsTools/TheNtupleMaker/interface/pdg.h"
+#else
+#include "pdg.h"
+#endif
+
+#include "CommonHistoCollection.h"
+#include "selection.h"
+#include "analyzer.h"
+
+// Structs useful for Analyzer
+
+
+
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  // Get file list and histogram filename from command line
+
+  commandLine cmdline;
+  decodeCommandLine(argc, argv, cmdline);
+
+  // Get names of ntuple files to be processed and open chain of ntuples
+
+  vector<string> filenames = getFilenames(cmdline.filelist);
+  itreestream stream(filenames, "Events");
+  if ( !stream.good() ) error("unable to open ntuple file(s)");
+
+  // Get number of events to be read
+
+  //int nevents = 100;
+  int nevents = stream.size();
+  cout << "Number of events: " << nevents << endl;
+
+  // Select variables to be read
+
+  selectVariables(stream);
+
+
+  // The root application is needed to make canvases visible during
+  // program execution. If this is not needed, just comment out the following
+  // line
+
+  TApplication app("analyzer", &argc, argv);
+
+  /*
+	 Notes:
+	
+	 1. Use
+	   ofile = outputFile(cmdline.outputfile, stream)
+	
+	 to skim events to output file in addition to writing out histograms.
+	
+	 2. Use
+	   ofile.addEvent(event-weight)
+	
+	 to specify that the current event is to be added to the output file.
+	 If omitted, the event-weight is defaulted to 1.
+	
+	 3. Use
+	    ofile.count(cut-name, event-weight)
+	
+	 to keep track, in the count histogram, of the number of events
+	 passing a given cut. If omitted, the event-weight is taken to be 1.
+	 If you want the counts in the count histogram to appear in a given
+	 order, specify the order, before entering the event loop, as in
+	 the example below
+	 
+	    ofile.count("NoCuts", 0)
+		ofile.count("GoodEvent", 0)
+		ofile.count("Vertex", 0)
+		ofile.count("MET", 0)
+  */
+  
+  outputFile ofile(cmdline.outputfilename);
+  ofile.count("NoCuts",0.);
+
+  //---------------------------------------------------------------------------
+  // Declare histograms
+  //---------------------------------------------------------------------------
+
+  TH2F* h2_Trigger = new TH2F("h2_Trigger", "triggered in isolations", 5, -1.5, 3.5, 5, -1.5, 3.5);
+  h2_Trigger->GetXaxis()->SetTitle("real #tau iso, 0Tight, 1Medium, 2Loose, 3NoIso");
+  h2_Trigger->GetYaxis()->SetTitle("fake #tau iso, 0Tight, 1Medium, 2Loose, 3NoIso");
+  h2_Trigger->Sumw2(); 
+  
+  TH2F* h2_NoTrigger = new TH2F("h2_NoTrigger", "NOT triggered in isolations", 5, -1.5, 3.5, 5, -1.5, 3.5);
+  h2_NoTrigger->GetXaxis()->SetTitle("real #tau iso, 0Tight, 1Medium, 2Loose, 3NoIso");
+  h2_NoTrigger->GetYaxis()->SetTitle("fake #tau iso, 0Tight, 1Medium, 2Loose, 3NoIso");
+  h2_NoTrigger->Sumw2();
+
+  //---------------------------------------------------------------------------
+  // Histogram Collection Init
+  //---------------------------------------------------------------------------
+	TH1::SetDefaultSumw2();
+	double weight = 1.;
+
+	MyEventCollection baselineObjectSelectionCollection ("baselineObjectSelection");
+	MyEventCollection TauTightIsoObjectSelectionCollection ("TauTightIsoObjectSelection");
+	MyEventCollection Tau1TightIsoObjectSelectionCollection ("Tau1TightIsoObjectSelection");
+	MyEventCollection TauMediumIsoObjectSelectionCollection ("TauMediumIsoObjectSelection");
+	MyEventCollection TauLooseIsoObjectSelectionCollection ("TauLooseIsoObjectSelection");
+	MyEventCollection TauNoIsoObjectSelectionCollection ("TauNoIsoObjectSelection");
+
+  //---------------------------------------------------------------------------
+  // Loop over events
+  //---------------------------------------------------------------------------
+
+  for(int entry=0; entry < nevents; ++entry)
+	{
+	  // Read event into memory
+	  stream.read(entry);
+
+	  // Uncomment the following line if you wish to copy variables into
+	  // structs. See the header file analyzer.h to find out what structs
+	  // are available. Each struct contains the field "selected", which
+	  // can be set as needed. Call saveSelectedObjects() before a call to
+	  // addEvent if you wish to save only the selected objects.
+	  
+	  fillObjects();
+           
+	  //if(entry%1000==0)
+	  //cout << "--processing entry " << entry << " of " << nevents << endl;
+	  // ----------------------
+	  // -- object selection --
+	  // ----------------------
+	  
+	  
+	//search for gentaus
+	std::vector<genparticlehelper_s*> genTau;  
+	  
+        for (unsigned int g = 0; g < genparticlehelper.size(); g++){
+		  if (  !(fabs(genparticlehelper[g].status)==3)  ) continue;
+		  if (fabs(genparticlehelper[g].pdgId) == 15) genTau.push_back(&genparticlehelper[g]);
+
+		}
+	
+    if(genTau.size()==1){
+	
+	
+        // vertex selection
+	bool goodVertex = true;
+
+	if(!( vertex.size() > 0 )) goodVertex = false;
+	if( goodVertex ) {
+		baselineObjectSelectionCollection.goodVertex = true;
+		TauTightIsoObjectSelectionCollection.goodVertex = true;
+		Tau1TightIsoObjectSelectionCollection.goodVertex = true;
+		TauMediumIsoObjectSelectionCollection.goodVertex = true;
+		TauLooseIsoObjectSelectionCollection.goodVertex = true;
+		TauNoIsoObjectSelectionCollection.goodVertex = true;
+	}
+	
+	;
+
+	
+          //trigger selection
+   
+          if(
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Prong1_v3 == 1) ||
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Prong1_v4 == 1) ||
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v2 == 1) ||
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v3 == 1) ||
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v4 == 1) ||
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v6 == 1)
+
+            )baselineObjectSelectionCollection.passedTrigger = true;
+
+	std::vector<int> tights;
+	std::vector<int> mediums;
+	std::vector<int> looses;
+	std::vector<int> nones;
+	
+	int realTau=-1;
+	
+          //smart tau selection
+	  for(unsigned int t =0;t<tau.size();++t){
+	    
+            if(!(	fabs(tau[t].eta) <= 2.1                              					)) continue;
+            if(!(       tau[t].pt >= 45.                                            				)) continue;
+            if(!(       tau[t].leadPFChargedHadrCand_pt >= 5.0                      				)) continue;
+            if(!(       tau[t].tauID_againstElectronTightMVA5 > 0.5                				)) continue;
+            if(!(       tau[t].tauID_againstMuonTight2 > 0.5                        				)) continue;
+            if(!(       (tau[t].tauID_decayModeFinding > 0.5) && (tau[t].signalPFChargedHadrCands_size == 1)	)) continue;
+	    double dR=deltaR(tau[t].eta, tau[t].phi, genTau[0]->eta, genTau[0]->phi);
+            if(dR<0.3){
+	      if(!(tau[t].tauID_byTightCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) realTau=0;
+	      else if(!(tau[t].tauID_byMediumCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) realTau=1;
+	      else if(!(tau[t].tauID_byLooseCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) realTau=2;
+	      else realTau=3;
+	    }
+	    else{
+	      baselineObjectSelectionCollection.tau.push_back(&tau[t]);
+	      if(!(tau[t].tauID_byTightCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) tights.push_back(t);
+	      else if(!(tau[t].tauID_byMediumCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) mediums.push_back(t);
+	      else if(!(tau[t].tauID_byLooseCombinedIsolationDeltaBetaCorr3Hits  <= 0.5)) looses.push_back(t);
+	      else nones.push_back(t);
+	    }
+          }
+          
+	  if(tights.size()==2) for(unsigned int t =0;t<tights.size();++t) {int i=tights[t]; TauTightIsoObjectSelectionCollection.tau.push_back(&tau[i]);}
+	  else if(tights.size()==1 && (mediums.size()+looses.size()+nones.size())==1) {tights.insert(tights.end(),mediums.begin(), mediums.end()); tights.insert(tights.end(),looses.begin(), looses.end()); tights.insert(tights.end(),nones.begin(), nones.end()); for(unsigned int t =0;t<tights.size();++t) {int i=tights[t]; Tau1TightIsoObjectSelectionCollection.tau.push_back(&tau[i]);}}
+	  else if(mediums.size()>=1 && (mediums.size()+looses.size()+nones.size())==2) {mediums.insert(mediums.end(), looses.begin(), looses.end()); mediums.insert(mediums.end(), nones.begin(), nones.end()); for(unsigned int t =0;t<mediums.size();++t) {int i=mediums[t]; TauMediumIsoObjectSelectionCollection.tau.push_back(&tau[i]);}}
+	  else if(looses.size()>=1 && (looses.size()+nones.size())==2) {looses.insert(looses.end(), nones.begin(), nones.end()); for(unsigned int t =0;t<looses.size();++t) {int i=looses[t]; TauLooseIsoObjectSelectionCollection.tau.push_back(&tau[i]);}}
+	  else if(nones.size()==2) for(unsigned int t =0;t<nones.size();++t) {int i=nones[t]; TauNoIsoObjectSelectionCollection.tau.push_back(&tau[i]);}
+	
+	int fakeTau=-1;
+	if(tights.size()==1) fakeTau=0;
+	else if(mediums.size()==1) fakeTau=1;
+	else if(looses.size()==1) fakeTau=2;
+	else if(nones.size()==1) fakeTau=3;
+	  
+	if(baselineObjectSelectionCollection.passedTrigger) h2_Trigger->Fill(realTau, fakeTau);
+	else h2_NoTrigger->Fill(realTau, fakeTau);
+    }
+	  //Event Count
+	  ofile.count("NoCuts");
+	  // ------------------------
+	  // -- Skimming Studies   --
+	  // ------------------------
+
+//Clearing Object Collections
+TauTightIsoObjectSelectionCollection.clear();
+Tau1TightIsoObjectSelectionCollection.clear();
+TauMediumIsoObjectSelectionCollection.clear();
+TauLooseIsoObjectSelectionCollection.clear();
+TauNoIsoObjectSelectionCollection.clear();
+baselineObjectSelectionCollection.clear();
+}
+  stream.close();
+  ofile.close();
+  return 0;
+}
+
