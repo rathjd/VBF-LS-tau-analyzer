6736798625bfd418ec1d8ec6d7f4315c3f17ec61
Denis Rathjens <Denis.Rathjens@cern.ch>
2014-06-04 15:29:10 +0200

    reimplemented missing trigger path Trk1_eta2p1_Prong1_v1

--- analyzer.cc
+++ analyzer.cc
@@ -178,8 +178,6 @@ int main(int argc, char** argv)
 if(!eventhelper_isRealData){
  weight=PUweights->GetBinContent(PUweights->FindBin(PileupSummaryInfo_getTrueNumInteractions[0]));
  //std::cout<<"NVtx="<<PileupSummaryInfo_getTrueNumInteractions[0]<<", weight="<<weight<<std::endl;
- //weight=PUweights->GetBinContent(PUweights->FindBin(PileupSummaryInfo_getPU_NumInteractions[0]));
- //std::cout<<"NVtx="<<PileupSummaryInfo_getPU_NumInteractions[0]<<", weight="<<weight<<std::endl;
 } 
 
 baselineObjectSelectionCollection.NVtx 		= nrecoVertex;
@@ -217,8 +215,8 @@ if( goodVertex ) {
              (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v2 == 1) ||
              (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v3 == 1) ||
              (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v4 == 1) ||
-             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v6 == 1)
-	     //|| (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Prong1_v1 == 1)
+             (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk5_eta2p1_Prong1_v6 == 1) ||
+	     (triggerresultshelper_value_HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Prong1_v1 == 1)
 
             ) { 	baselineObjectSelectionCollection.passedTrigger = true;	
             		TauTightIsoObjectSelectionCollection.passedTrigger = true;
