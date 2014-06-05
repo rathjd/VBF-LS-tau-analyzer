ed56b3345090d922957200dbbddf80930aa7cd47
Denis Rathjens <Denis.Rathjens@cern.ch>
2014-06-05 16:25:35 +0200

    added missing x axis descriptors

--- include/structures.h
+++ include/structures.h
@@ -128,45 +128,64 @@ struct MyHistoCollection {
 		h_njet = new TH1F("h_njet", "h_njet", 21, -0.5, 20.5);
 		h_njet->GetXaxis()->SetTitle("number of jets not matched to #tau");
 		h_jetpt = new TH1F("h_jetpt", "h_jetpt", 50, 0., 500.);
-		h_jetpt->GetXaxis()->SetTitle("p_{T}^{jet}");
+		h_jetpt->GetXaxis()->SetTitle("p_{T}^{jet} [GeV]");
 		h_jeteta = new TH1F("h_jeteta", "h_jeteta", 30 , -5., 5.);
 		h_jeteta->GetXaxis()->SetTitle("#eta^{jet}");
 		h_jet1pt = new TH1F("h_jet1pt", "h_jet1pt", 50, 0., 500.);
-		h_jet1pt->GetXaxis()->SetTitle("p_{T}^{jet 1}");
+		h_jet1pt->GetXaxis()->SetTitle("p_{T}^{jet 1} [GeV]");
 		h_jet1eta = new TH1F("h_jet1eta", "h_jet1eta", 50 , -5., 5.);
 		h_jet1eta->GetXaxis()->SetTitle("#eta^{jet 1}");
 		h_jet2pt = new TH1F("h_jet2pt", "h_jet2pt", 50, 0., 500.);
-		h_jet2pt->GetXaxis()->SetTitle("p_{T}^{jet 2}");
+		h_jet2pt->GetXaxis()->SetTitle("p_{T}^{jet 2} [GeV]");
 		h_jet2eta = new TH1F("h_jet2eta", "h_jet2eta", 50 , -5., 5.);
 		h_jet2eta->GetXaxis()->SetTitle("#eta^{jet 2}");
 		h_dijetinvariantmass = new TH1F("h_dijetinvariantmass","h_dijetinvariantmass", 10, 0., 2500.);
+		h_dijetinvariantmass->GetXaxis()->SetTitle("M^{(jet,jet)} [GeV]");
 		h_dijetdeltaeta = new TH1F ("h_dijetdeltaeta", "h_dijetdeltaeta", 20, 0., 10.);
 		h_dijetdeltaeta->GetXaxis()->SetTitle("#Delta#eta^{jj}");
 
 		h_tau1pt = new TH1F("h_tau1pt", "h_tau1pt", 50, 0., 500.);
+		h_tau1pt->GetXaxis()->SetTitle("p_{T}^{#tau 1} [GeV]");
 		h_tau1eta = new TH1F("h_tau1eta", "h_tau1eta", 30 , -3., 3.);
+		h_tau1eta->GetXaxis()->SetTitle("#eta^{#tau 1}");
 		h_tau2pt = new TH1F("h_tau2pt", "h_tau2pt", 50, 0., 500.);
+		h_tau2pt->GetXaxis()->SetTitle("p_{T}^{#tau 2} [GeV]");
 		h_tau2eta = new TH1F("h_tau2eta", "h_tau2eta", 30 , -3., 3.);
+		h_tau2eta->GetXaxis()->SetTitle("#eta^{#tau 2}");
 		h_ditauinvariantmass = new TH1F("h_ditauinvariantmass", "h_ditauinvariantmass", 50, 0., 500.);
+		h_ditauinvariantmass->GetXaxis()->SetTitle("M^{(#tau,#tau)} [GeV]");
 		h_ditaucharge = new TH1F("h_ditaucharge", "h_ditaucharge", 5, -4., 6.);
+		h_ditaucharge->GetXaxis()->SetTitle("sign(#tau^{1}) #upoint sign(#tau^{2})");
 		h_ditaucosdeltaphi = new TH1F("h_ditaucosdeltaphi", "h_ditaucosdeltaphi", 50, -1.1, 1.1);
+		h_ditaucosdeltaphi->GetXaxis()->SetTitle("cos(#Delta#phi(#tau,#tau))");
 		h_ditaudeltaeta = new TH1F ("h_ditaudeltaeta", "h_ditaudeltaeta", 20, 0., 10.);
+		h_ditaudeltaeta->GetXaxis()->SetTitle("#Delta#eta(#tau,#tau)");
 
 		h_met = new TH1F("h_met", "h_met", 24, 0., 240.);
+		h_met->GetXaxis()->SetTitle("E_{T}^{miss} [GeV]");
 
 		h_ht = new TH1F("h_ht", "h_ht", 50, 0., 1300.);
+		h_ht->GetXaxis()->SetTitle("H_{T} [GeV]");
 		h_ht_withtau = new TH1F("h_ht_withtau", "h_ht_withtau", 50, 0., 1300.);
+		h_ht_withtau->GetXaxis()->SetTitle("H_{T}+#Sigma p_{T}^{#tau} [GeV]");
 		
 		h_jetTauDistanceFirst = new TH1F("h_jetTauDistanceFirst", "h_jetTauDistanceFirst", 25, 0., 0.5);
+		h_jetTauDistanceFirst->GetXaxis()->SetTitle("#DeltaR(jet,#tau^{1})");
 		h_jetTauDistanceSecond = new TH1F("h_jetTauDistanceSecond", "h_jetTauDistanceSecond", 25, 0., 0.5);
+		h_jetTauDistanceSecond->GetXaxis()->SetTitle("#DeltaR(jet,#tau^{2})");
 
 		h2_DiJetInvMass_vs_DiJetDEta = new TH2F("h2_DiJetInvMass_vs_DiJetDEta","h2_DiJetInvMass_vs_DiJetDEta", 20, 0., 10., 10, 0., 2500.);
+		h2_DiJetInvMass_vs_DiJetDEta->GetXaxis()->SetTitle("#Delta#eta^{jj}");
+		h2_DiJetInvMass_vs_DiJetDEta->GetYaxis()->SetTitle("M^{(jet,jet)} [GeV]");
 		h2_tau1pt_vs_tau2pt = new TH2F("h2_tau1pt_vs_tau2pt","correlation of first and second p_{T}^{#tau}", 50, 0., 500., 50, 0., 500.);
 		h2_tau1pt_vs_tau2pt->GetXaxis()->SetTitle("p_{T}^{#tau 1}");
 		h2_tau1pt_vs_tau2pt->GetYaxis()->SetTitle("p_{T}^{#tau 2}");
 		
 		h_NVtx = new TH1F("h_NVtx","number of reco vertices", 100, -0.5,99.5);
-		h_PU_NumInteractions = new TH1F("h_PU_NumInteractions","number of in-time pileup", 100, -0.5,99.5);
+		h_NVtx->GetXaxis()->SetTitle("NVtx");
+		
+		h_PU_NumInteractions = new TH1F("h_PUTrueInteractions","number of in-time true pileup", 100, -0.5,99.5);
+		h_PU_NumInteractions->GetXaxis()->SetTitle("NVtx^{true}");
 	}
 };
 
