e56bcad5693b2e9b47286cd135291d20247982fb
Daniele <daniele.marconi87@gmail.com>
2014-07-10 22:07:30 +0200

    Added MC study, added saving system

--- macro/mjjshape_study.C
+++ macro/mjjshape_study.C
@@ -23,7 +23,7 @@ struct sampleplots_str {
 	TH1F* h1_CR8plot;
 };
 
-sampleplots_str* addsamplesplots(sampleplots_str *sample_sampleplots_str1, sampleplots_str *sample_sampleplots_str2) {
+void addsamplesplots(sampleplots_str *sample_sampleplots_str1, sampleplots_str *sample_sampleplots_str2) {
 
 	sample_sampleplots_str1->h1_SRplot->Add(sample_sampleplots_str2->h1_SRplot);
 	sample_sampleplots_str1->h1_CR2plot->Add(sample_sampleplots_str2->h1_CR2plot);
@@ -33,8 +33,6 @@ sampleplots_str* addsamplesplots(sampleplots_str *sample_sampleplots_str1, sampl
 	sample_sampleplots_str1->h1_CR6plot->Add(sample_sampleplots_str2->h1_CR6plot);
 	sample_sampleplots_str1->h1_CR7plot->Add(sample_sampleplots_str2->h1_CR7plot);
 	sample_sampleplots_str1->h1_CR8plot->Add(sample_sampleplots_str2->h1_CR8plot);
-
-	return sample_sampleplots_str1;
 
 }
 
@@ -96,6 +94,9 @@ void mjjshapestudy(bool isLSchannel, string plotname) {
 	sampleplots_str Data_sampleplots;
 	Data_sampleplots.samplename = "Data";
 
+	sampleplots_str AllMC_sampleplots;
+	AllMC_sampleplots.samplename = "AllMC";
+
 	sampleplots_str DY_sampleplots;
 	DY_sampleplots.samplename = "Drell-Yan";
 
@@ -117,6 +118,7 @@ void mjjshapestudy(bool isLSchannel, string plotname) {
 	sampleplots_str QCD_sampleplots;
 	QCD_sampleplots.samplename = "QCD";
 
+	mjjplots_init(_allDY, &AllMC_sampleplots, isLSchannel, plotname);
 	mjjplots_init(_allDY, &DY_sampleplots, isLSchannel, plotname);
 	mjjplots_init(_allVV, &VV_sampleplots, isLSchannel, plotname);
 	mjjplots_init(_allW, &W_sampleplots, isLSchannel, plotname);
@@ -125,17 +127,28 @@ void mjjshapestudy(bool isLSchannel, string plotname) {
 	mjjplots_init(_allHiggs, &Higgs_sampleplots, isLSchannel, plotname);
 	mjjplots_init(_allQCD, &QCD_sampleplots, isLSchannel, plotname);
 	mjjplots_init(_allData, &Data_sampleplots, isLSchannel, plotname);
+	
 	normalizesampleplots (&Data_sampleplots);
 
-	//TCanvas* c1 = new TCanvas("c1","c1",1,1);
-	//c1->cd();
+	addsamplesplots(&AllMC_sampleplots , &VV_sampleplots);
+	addsamplesplots(&AllMC_sampleplots , &W_sampleplots);
+	addsamplesplots(&AllMC_sampleplots , &T_sampleplots);
+	addsamplesplots(&AllMC_sampleplots , &TTbar_sampleplots);
+	addsamplesplots(&AllMC_sampleplots , &Higgs_sampleplots);
+	addsamplesplots(&AllMC_sampleplots , &QCD_sampleplots);
+	normalizesampleplots (&AllMC_sampleplots);
+
+
+	TCanvas* c1 = new TCanvas("c1","c1",1);
+	c1->cd();
 
 	Data_sampleplots.h1_CR3plot->SetStats(0);
+	Data_sampleplots.h1_CR3plot->SetTitle("M_{jj} distribution for different #tau isolation regions");
 	Data_sampleplots.h1_CR5plot->SetLineColor(kBlack);	
 	Data_sampleplots.h1_CR3plot->Draw();
 	Data_sampleplots.h1_CR5plot->SetLineColor(kRed);	
 	Data_sampleplots.h1_CR5plot->Draw("same");	
-	Data_sampleplots.h1_CR7plot->SetLineColor(kBlue);	
+	Data_sampleplots.h1_CR7plot->SetLineColor(kGreen);	
 	Data_sampleplots.h1_CR7plot->Draw("same");
 
 	TLegend* legdata = new TLegend(0.6,0.65,0.88,0.85);
@@ -144,7 +157,26 @@ void mjjshapestudy(bool isLSchannel, string plotname) {
 	legdata->AddEntry(Data_sampleplots.h1_CR7plot, "Data CR7", "l");
 	legdata->Draw("same");
 
-	//c1->SaveAs("mjjshapestab_vs_tauiso.pdf");	
+	c1->SaveAs("mjjshapestab_vs_tauiso_data.pdf");
+	c1->Clear();
+
+	AllMC_sampleplots.h1_CR3plot->SetStats(0);
+	AllMC_sampleplots.h1_CR3plot->SetTitle("M_{jj} distribution for different #tau isolation regions");
+	AllMC_sampleplots.h1_CR5plot->SetLineColor(kBlack);	
+	AllMC_sampleplots.h1_CR3plot->Draw();
+	AllMC_sampleplots.h1_CR5plot->SetLineColor(kRed);	
+	AllMC_sampleplots.h1_CR5plot->Draw("same");	
+	AllMC_sampleplots.h1_CR7plot->SetLineColor(kGreen);	
+	AllMC_sampleplots.h1_CR7plot->Draw("same");
+
+	TLegend* legmc = new TLegend(0.6,0.65,0.88,0.85);
+	legmc->AddEntry(AllMC_sampleplots.h1_CR3plot, "all MC CR3", "l");
+	legmc->AddEntry(AllMC_sampleplots.h1_CR5plot, "all MC CR5", "l");
+	legmc->AddEntry(AllMC_sampleplots.h1_CR7plot, "all MC CR7", "l");
+	legmc->Draw("same");
+
+	c1->SaveAs("mjjshapestab_vs_tauiso_mc.pdf");
+	c1->Clear();
 
 }
 
